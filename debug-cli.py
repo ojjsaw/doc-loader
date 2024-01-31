d56007947cdd099912d0f331dda970dfb231ad3c
Ojas D. Sawant <ojjsaw@gmail.com>
2024-01-31 00:51:22 +0000

    python token

--- debug-cli.py
+++ debug-cli.py
@@ -18,6 +18,9 @@ from langchain_core.prompts import PromptTemplate
 from langchain.chains import RetrievalQA
 from chromadb.config import Settings
 
+os.environ['HF_TOKEN'] = "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"
+
+
 template = """Use the following pieces of context to answer the question at the end.
 If you don't know the answer, just say that you don't know, don't try to make up an answer.
 Use three sentences maximum and keep the answer as concise as possible.
